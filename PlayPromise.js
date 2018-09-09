e101dec9154207097cd64b4538bff66edff3011f
Steven Shao <stevenshao1900@icloud.com>
2018-09-09 10:26:31 +0800

    checkin

---
+++ PlayPromise.js
@@ -0,0 +1,24 @@
+
+var p = new Promise(function(resolve, reject){
+    //throw "123";
+
+    setTimeout(function(){
+        //throw "254";
+        resolve(72);
+    },1000)
+    //resolve(45);
+    
+    //reject("helloworld");
+});
+
+
+
+p.then(function fulfill(data){
+    console.log("rabit1" + data);
+},function reject(err){
+    throw err;
+    console.log("rabit2" + err)
+
+}).catch(function handle(err){
+    console.log("rabit3" + err)
+});
